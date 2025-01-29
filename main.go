e1b5863db8a31c95899e83c417d52dabfa96a85f
Josip Ledic <Josip.Ledic@ibm.com>
2025-01-29 11:47:24 +0100

    cleanup

--- main.go
+++ main.go
@@ -8,26 +8,21 @@ import (
 	"time"
 )
 
-// Check the external API connection
 func checkConnection(w http.ResponseWriter, r *http.Request) {
 	// Default to 8.8.8.8, but use EXTERNAL_API environment variable if set
 	externalAPI := os.Getenv("EXTERNAL_API")
 	if externalAPI == "" {
-		externalAPI = "8.8.8.8" // default
+		externalAPI = "8.8.8.8"
 	}
 
-	// Set a timeout for the connection attempt
 	client := http.Client{
-		Timeout: 5 * time.Second,
+		Timeout: 10 * time.Second,
 	}
 
-	// Prepare the message to send back in the response
 	var responseMessage string
 
-	// Log the incoming request for debugging
-	log.Printf("Incoming request from: %s", r.RemoteAddr)
+	log.Printf("Incoming request")
 
-	// Make the GET request to the external API
 	resp, err := client.Get("https://" + externalAPI)
 	if err != nil {
 		// If we can't connect, return an error message
@@ -38,25 +33,23 @@ func checkConnection(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, responseMessage, http.StatusServiceUnavailable)
 		return
 	}
-	defer resp.Body.Close()
 
 	// If we get a response, check the status code
 	if resp.StatusCode != 200 {
 		log.Printf("Received non-OK status code from %s: %d", externalAPI, resp.StatusCode)
-		responseMessage = fmt.Sprintf("Error: Received non-OK status code %d from external API %s\nDetails: Expected 200, but got %d\n", externalAPI, resp.StatusCode, resp.StatusCode)
+		responseMessage = fmt.Sprintf("Error: Received non-OK status code %d from external API %s\n\n", externalAPI, resp.StatusCode)
 
-		// Return the non-200 status code
 		http.Error(w, responseMessage, http.StatusServiceUnavailable)
 		return
 	}
 
-	// If successful, prepare the success message
-	responseMessage = fmt.Sprintf("Success: Successfully connected to external API %s\nResponse Status: %d\nDetails: Received a 200 OK response.\n", externalAPI, resp.StatusCode)
+	responseMessage = fmt.Sprintf("Successfully connected to external API %s\n Status: %d\n\n", externalAPI, resp.StatusCode)
 
-	// Set the content type and write the response message
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(responseMessage))
+
+	defer resp.Body.Close()
 }
 
 func main() {
